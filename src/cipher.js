08fe1edf6d4c8ac115b8ce8af7f90107e75cfeea
valeskaperezrretamal <jperezr@udd.cl>
2019-02-08 19:55:45 -0300

    se define la función cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,17 @@
-window.cipher = {
-    encode: () => {
-      /* Acá va tu código */
-    },
-    decode: () => {
-      /* Acá va tu código */
-    }
-  };
+function cipher (offset,text){
+	let txtcifrado = "";
+	let AsciiCesar = 0;
+    let formula = 0;
+      for (let i= 0; i< text.length; i++){
+      AsciiCesar = text.charCodeAt(i);
+      formula = String.fromCharCode((AsciiCesar - 65 + offset) % 26 + 65);
+      txtcifrado= txtcifrado + formula;
+      }
+      return txtcifrado;
+
+}
+ 
+   
+
+  
+ 
