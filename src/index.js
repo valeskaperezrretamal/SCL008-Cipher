4a37003a065442dfb6804880c5a7b03c59766364
valeskaperezrretamal <jperezr@udd.cl>
2019-02-08 19:58:10 -0300

    se define el evento para agregar funcionalidad al botón codificar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-/* Acá va tu código */
+/*------------------------------------------------------------------------
+-----------------------------definición de variables--------------------
+--------------------------------------------------------------------------*/
+
+
+const botoncodificar = document.getElementById ("botoncodificar");
+
+const botondecodificar = document.getElementById ("botondecodificar");
+
+
+
+/*-------------------------definición de eventos----------------------------*/
+botoncodificar.addEventListener("click", () => {
+/*codigo para guardar en una variable, el contenido de caja texto original*/
+const textoOriginal = document.getElementById("cajaTextoOriginal").value
+
+/*codificar el texto usando cipher y guardarlo en una nueva varieble*/
+
+const textoCodificado = cipher(2,textoOriginal);
+
+/*escribir el texto codificado en la caja texto  codificado*/
+document.getElementById("cajaTextoCodificado").value = textoCodificado;
+})
+
+botondecodificar.addEventListener("click", () => {
+    /*agregar codigo para guardar en una variable, el contenido de caja texto caja codificado*/
+    
+    
+    /*decodificar el texto usando cipher decode*/
+    
+    /*escribir el texto decodificado en la caja texto original*/
+    })
