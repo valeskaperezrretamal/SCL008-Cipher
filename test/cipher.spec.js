c6c993b5cd2afd06bad85a11e410e5d77c10f552
valeskaperezrretamal <jperezr@udd.cl>
2019-02-12 00:14:27 -0300

    se creo test, para el objeto cipher y sus metodos, donde pasó 100% en todas sus categorias

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,7 +10,22 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+      assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
+    });
+
+    it('debería retornar "fghijklmnopqrstuvwxyzabcde" para "abcdefghijklmnopqrstuvwxyz" con offset 5', () => {
+      assert.equal(cipher.encode(5,"abcdefghijklmnopqrstuvwxyz"), "fghijklmnopqrstuvwxyzabcde");
+    });
+    it('debería retornar "1234567890" para "0123456789" con offset 51', () => {
+      assert.equal(cipher.encode(51,"0123456789"), "1234567890");
+    });
+    it('debería retornar "¡ubyn YnObEnGbEvN! rfgr rf zv ceblrpgb 8" para "¡hola LaBoRaToRiA! este es mi proyecto 1" con offset -13', () => {
+      assert.equal(cipher.encode(-13, "¡hola LaBoRaToRiA! este es mi proyecto 1"),"¡ubyn YnObEnGbEvN! rfgr rf zv ceblrpgb 8");
+    });
+  
+  
+  
   });
 
   describe('cipher.decode', () => {
@@ -19,7 +34,19 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
+      assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "fghijklmnopqrstuvwxyzabcde" con offset 5', () => {
+      assert.equal(cipher.decode(5,"fghijklmnopqrstuvwxyzabcde"), "abcdefghijklmnopqrstuvwxyz");
+    });
+    it('debería retornar "0123456789" para "1234567890" con offset 51', () => {
+      assert.equal(cipher.decode(51,"1234567890"), "0123456789");
+    });
+    it('debería retornar "¡hola LaBoRaToRiA! este es mi proyecto 1" para "¡ubyn YnObEnGbEvN! rfgr rf zv ceblrpgb 8" con offset -13', () => {
+      assert.equal(cipher.decode(-13, "¡ubyn YnObEnGbEvN! rfgr rf zv ceblrpgb 8"),"¡hola LaBoRaToRiA! este es mi proyecto 1");
+    });
+    
   });
 
 });
