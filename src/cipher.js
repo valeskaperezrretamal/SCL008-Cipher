f4ef17049c2c98fb015411a71031514a16ef3c17
valeskaperezrretamal <jperezr@udd.cl>
2019-02-12 00:06:49 -0300

    cipher actualizado, se creo objeto cipher con sus metodos encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,43 @@
-  // la función cipher aplica el cifrado cesar a las letras mayusculas, minusculas y numeros. Devuelve una cadena de texto cifrada en donde los elementos no cifrados no son modificados
-  //string Soporta todo tipo de cadena de texto.
-  //offset, soporta valores positivos y negativos, corrimiento a la derecha e izquierda respectivamente.
-  //para decifrar basta con invertir el signo del offset
-  function cipher(offset,string){
+//para eviar escribir dos vces el mismo codigo se definio una funcion que luego es usada dentro del objeto cipher.
+
+window.cipher = {
+  encode: (offset, mensaje)=> {
+    return cifrar(offset,mensaje);
+  }
+   ,
+
+   decode: (offset, mensaje)=> {
+      return cifrar(-offset,mensaje);
+
+   }
+}
+
+// la función cipher aplica el cifrado cesar a las letras mayusculas, minusculas y numeros. Devuelve una cadena de texto cifrada en donde los elementos no cifrados no son modificados
+//string Soporta todo tipo de cadena de texto.
+//offset, soporta valores positivos y negativos, corrimiento a la derecha e izquierda respectivamente.
+//para decifrar basta con invertir el signo del offset
+
+
+function cifrar(offset,mensaje){
   let txtcifrado = "";
   let caracterAscii;
   let formula;
   let aux;
   let tipoDeCaracter;
-  /*la función for recorre la cadena de texto*/
-  for (let i= 0; i< string.length; i++){
-    caracterAscii = string.charCodeAt(i); /* variable que guarda el carcater transformado en ascii*/
+  //la función for recorre la cadena de texto
+  for (let i= 0; i< mensaje.length; i++){
+    caracterAscii = mensaje.charCodeAt(i);  //variable que guarda el carcater transformado en ascii
     
-    /*se revisa que tipo de caracter que se va a codificar, puede ser mayuscula, minuscula, numero, espacio u otros*/
-    tipoDeCaracter = "otro"; 
+    /*se revisa que tipo de caracter que se va a codificar, puede ser mayuscula, minuscula, numero, 
+    espacio u otros*/
+    tipoDeCaracter = "otro"
     if (caracterAscii >= 65 && caracterAscii <= 90) { tipoDeCaracter = "mayuscula";}
     if (caracterAscii >= 97 && caracterAscii <= 122) { tipoDeCaracter = "minuscula";}
     if (caracterAscii >= 48 && caracterAscii <= 57) { tipoDeCaracter = "numero";}
     
     
     
-    /*para mayusculas se usará esta formula*/
+    //para mayusculas se usará esta formula
     if (tipoDeCaracter === "mayuscula") {
       /*la implementacion del operador residuo provoca problemas en la formula original 
       cuando el dividendo es negativo, por este motivo al resultado del primer residuo le sumamos 26 
@@ -31,21 +48,21 @@
       formula = String.fromCharCode((aux+26) % 26 + 65);
       txtcifrado= txtcifrado + formula;
     }
-    /*para minusculas se usará esta formula*/
+    //para minusculas se usará esta formula
     if (tipoDeCaracter === "minuscula") {
       aux=(caracterAscii - 97 + offset) % 26;
       formula = String.fromCharCode((aux+26) % 26 + 97);
       txtcifrado= txtcifrado + formula;
     }
-    /*para numeros su usará esta formula*/
+    //para numeros su usará esta formula
     if (tipoDeCaracter === "numero") {
       aux=(caracterAscii - 48 + offset) % 10;
       formula = String.fromCharCode((aux+10) % 10 + 48);
       txtcifrado= txtcifrado + formula; 
     }
-    /*para otros se usará esta formula*/
+    //para otros se usará esta formula
     if (tipoDeCaracter === "otro") {
-      txtcifrado = txtcifrado + string[i];
+      txtcifrado = txtcifrado + mensaje[i];
     }
 
 
